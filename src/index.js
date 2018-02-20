0accbc625eb985dc08f010f274c65ca7b430c09e
raikaat <raikaat@gmail.com>
2018-02-20 23:32:59 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var arr = preferences;
+  var count = 0;
+  for (var i = 0; i < arr.length; i++){
+  	var a = arr[i];
+    var b = arr[a-1];
+    var c = arr[b-1];
+
+    if((i+1===c)&&(b!=i+1)){
+      count++;
+    } else {
+      continue;
+    }
+  }
+  	return count/3;
 };
